95d89fc5c3605f148c0e511e4faf6851c9d9c3e3
Lebohang <lebohangmokhwibitjane@gmail.com>
2025-01-08 01:23:37 +0200

    Init

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivymd.app import MDApp as App
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton as RectFlatButton
+from kivymd.uix.dialog import MDDialog as Dialog
+from kivy.lang import Builder
+
+
+if __name__ == "__main__":
+    print("Hello world")
